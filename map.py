0cc673a96d7d248e20fed0136fe29a0c721ea65f
KevJin97 <Kjin150@gmail.com>
2022-05-05 16:33:29 -0400

    fixed up

--- map.py
+++ map.py
@@ -5,30 +5,58 @@ Created on Thu May  5 03:12:36 2022
 
 """
 
-def N(data, i, j, k=0):
-    return data[i-1][j][k]
+def N(data, i, j, k=0, skipby=1):
+    if (k!=0):    
+        return data[skipby*i-1][j][k]
+    else:
+        return data[skipby*i-1][j]
 
-def S(data, i, j, k=0):
-    return data[i+1][j][k]
+def S(data, i, j, k=0, skipby=1):
+    if (k!=0):  
+        return data[skipby*i+1][j][k]
+    else:
+        return data[skipby*i+1][j]
 
-def E(data, i, j, k=0):
-    return data[i][j+1][k]
+def E(data, i, j, k=0, skipby=1):
+    if (k!=0):  
+        return data[i][skipby*j+1][k]
+    else:
+        return data[i][skipby*j+1]
 
-def W(data, i, j, k=0):
-    return data[i][j-1][k]
+def W(data, i, j, k=0, skipby=1):
+    if (k!=0):  
+        return data[i][skipby*j-1][k]
+    else:
+        return data[i][skipby*j-1]
 
-def NE(data, i, j, k=0):
-    return data[i-1][j+1][k]
+def NE(data, i, j, k=0, skipby=1):
+    if (k!=0):  
+        return data[i-1][skipby*j+1][k]
+    else:
+        return data[i-1][skipby*j+1]
 
-def NW(data, i, j, k=0):
-    return data[i-1][j-1][k]
+def NW(data, i, j, k=0, skipby=1):
+    if (k!=0):  
+        return data[skipby*i-1][skipby*j-1][k]
+    else:
+        return data[skipby*i-1][skipby*j-1]
+        
+    
+def SE(data, i, j, k=0, skipby=1):
+    if (k!=0):  
+        return data[skipby*i+1][skipby*j+1][k]
+    else:
+        return data[skipby*i+1][skipby*j+1]
 
-def SE(data, i, j, k=0):
-    return data[i+1][j+1][k]
+def SW(data, i, j, k=0, skipby=1):
+    if (k!=0):  
+        return data[skipby*i+1][skipby*j-1][k]
+    else:
+        return data[skipby*i+1][skipby*j-1]
 
-def SW(data, i, j, k=0):
-    return data[i+1][j-1][k]
-
-def cen(data, i, j, k=0):
-    return data[i][j][k]
+def cen(data, i, j, k=0, skipby=1):
+    if (k!=0):  
+        return data[i][j][k]
+    else:
+        return data[i][j]
 
