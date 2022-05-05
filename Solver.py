81ef66c2332997970350f98ebb735eb90acb2517
TheWhenCat <dhruva735@gmail.com>
2022-05-05 05:17:59 -0400

    Diagnolization is Up

--- Solver.py
+++ Solver.py
@@ -35,12 +35,58 @@ def Roe_Jacobian(eta, stateL, stateR, gamma = 1.4):
     lambda3 = vn + c
     lambda4 = lambda1
     
-    Rv = np.array([1, 1, 1, 0;
-                   u - c*etax, u, u + a*etax, etay; 
-                   v - c*etay, v, v + c*etay, -1*etax; 
-                   h - c*vn, 0.5*(u**2 + v**2), h + c*vn, u*etay - v*etax])
+    Rv = np.array([[1, 1, 1, 0],
+                   [u - c*etax, u, u + c*etax, etay],
+                   [v - c*etay, v, v + c*etay, -1*etax],
+                   [h - c*vn, 0.5*(u**2 + v**2), h + c*vn, u*etay - v*etax]])
     
-def state_interpolation():
+    eigval = [abs(vn - c), abs(vn), abs(vn + c), abs(vn)]
+    Diag = np.zero([len(eigval), len(eigval)])
+    for i in len(range(eigval)):
+        Diag[i, i] = eigval[i]
+        
+    g = (gamma-1)
+    ss = c**2
+    ek = 0.5*(u**2 + v**2)
+    Lv = np.array([[(g*ek + c*vn)/(2*ss), (-1*g*u - c*etax)*0.5*1/(ss), (-1*g*v - c*etay)*0.5*1/(ss), 0.5*g/(ss)],
+                   [(ss - g*ek)/ss, g*u/ss, g*v/ss, -1*g/ss],
+                   [(g*ek - c*vn)/(2*ss), (-1*g*u + c*etax)/(2*ss), (-1*g*v + c*etay)/(2*ss), (-1*g/(2*ss))],
+                   [(v - vn*etay)/etax, etay, -etax, 0]])
+    R = np.matmul(Diag, Lv)
+    out = np.matmul(Lv, Diag)
+    return out
+    
+def Convective_OperatorE():
     pass
 
+def Convective_OperatorF():
+    pass
+
+def phi(mode):
+    if mode == "const":
+        r = 1
+        phi = 1
+        return phi,r
+    if mode == "minmod":
+        return AttributeError("Dumbass You Need to Fix This")
+    
+    
+def state_interpolation(index, epsilon, kappa, direction, state):
+    orient = {"xi":0, "eta":1}
+    #i index is flipped in my grid
+    i = index[0]
+    j = index[1]
+    Q = state[i, j]
+    if orient[direction] == 0:
+        Qnegone = state[i - 1, j]
+        Qnegtwo = state[i - 3, j]
+        Qposone = state[i + 1, j]
+        
+    limiterL, rL = phi("const")
+    QL = Qnegone + 0.25*epsilon*((1 - kappa)*(Qnegone - Qnegtwo) * limiterL + (1 + kappa)*(Q - Qnegone)*1/rL * limiterL)
+    
+    limiterR, rR = phi("const")
+    QR = Q - 0.25*epsilon*((1 + kappa)*(Q - Qnegone)*limiterR*1/rR + (1 - kappa)*(Qposone - Q)*limiterR)
+    return QL, QR
+    
 
